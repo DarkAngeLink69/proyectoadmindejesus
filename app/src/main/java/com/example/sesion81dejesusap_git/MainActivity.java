0232dd92431e7c3d9372ff8b0acd54d67cbef668
DarkAngeLink69 <djva67@gmail.com>
2025-03-07 18:44:57 -0600

    s

--- app/src/main/java/com/example/sesion81dejesusap_git/MainActivity.java
+++ app/src/main/java/com/example/sesion81dejesusap_git/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.sesion81dejesusap_git;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button btnVenta, btnContacto, btnProducto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnVenta= findViewById(R.id.btnVenta);
+        btnVenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  abrirVenta = new Intent(getApplicationContext(), Venta.class);
+                startActivity(abrirVenta);
+            }
+        });
+        btnContacto=findViewById(R.id.btnContacto);
+        btnContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  abrirContacto = new Intent(getApplicationContext(), Contacto.class);
+                startActivity(abrirContacto);
+            }
+        });
+        btnProducto=findViewById(R.id.btnProductos);
+        btnProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  abrirProducto = new Intent(getApplicationContext(), Productos.class);
+                startActivity(abrirProducto);
+            }
         });
     }
 }
